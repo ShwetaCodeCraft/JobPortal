3dbcbee15a7fcb10ccce25bde59e9d9f51ff3fbd
Shweta Singh <142297936+ShwetaCodeCraft@users.noreply.github.com>
2025-11-12 16:08:53 +0530

    Delete frontend/backend directory

--- frontend/backend/middlewares/isRecruiter.js
+++
@@ -1,9 +0,0 @@
-const isRecruiter = (req, res, next) => {
-  if (req.user && req.user.role === "recruiter") {
-    return next();
-  }
-  return res.status(403).json({ message: "Access denied: Recruiter only" });
-};
-
-
-export default isRecruiter;

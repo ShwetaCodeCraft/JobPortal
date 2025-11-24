b81dec10d663ff4fbcb48d35678a6d759cb74421
Shweta Singh <singh.shweta2973@gmail.com>
2025-11-24 16:26:37 +0530

    homepage added

---
+++ frontend/src/CategoryCarousel.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+
+const category = [
+    "Frontend Developer",
+    "Backend Developer",
+    "Data Science",
+    "Graphic Designer",
+    "FullStack Developer"
+]
+ 
+export const CategoryCarousel = () => {
+  return (
+    <div>CategoryCarousel</div>
+  )
+}
